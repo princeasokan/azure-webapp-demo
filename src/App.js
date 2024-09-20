388a4cfdcf801c2d5e53e464eb4d485cbc8d59fb
prince <princekasok@gmail.com>
2024-09-20 17:13:41 +0530

    added .env

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  console.log(process.env.REACT_APP_API_URL);
+
   return (
     <div className="App">
       <header className="App-header">
